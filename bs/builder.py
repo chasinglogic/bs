7a023eb3ff64f1f51921c9511d8d68bde85466c2
Mathew Robinson <chasinglogic@gmail.com>
2020-02-04 17:26:36 -0500

    Define a constructor for Builder, create Program builder

--- bs/builder.py
+++ bs/builder.py
@@ -1,13 +1,33 @@
-from .node import Node
+from typing import List
+
+from bs.node import Node
+from bs.errors import UserError
 
 
 class Builder:
-    def get_command(self, targets: List[Node], sources: List[Node]):
+    def __init__(self, env, targets, sources, **overrides):
+        self.env = env
+        self.targets = targets
+        self.sources = sources
+        self.overrides = overrides
+
+    def get_command(self):
         raise NotImplementedError
 
 
-class Command(Builder):
-    def __init__(self, targets: List[Node], sources, cmdstr):
-        self.cmdstr = cmdstr
-        self.targets = targets
-        self.sources = sources
+class Program(Builder):
+    """Builds programs based on the file extension of sources."""
+
+    def get_command(self):
+        if not self.sources:
+            return []
+
+        extension = get_file_extension(self.sources[0])
+        try:
+            return self.env.command_map[extension]
+        except KeyError:
+            raise UserError(
+                "Unknown file extension for Program builder: {} for target {}".format(
+                    extension, self.targets
+                )
+            )
