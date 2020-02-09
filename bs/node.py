8d4d2e88a0b6b1f4500a5ec6b49c30229618333a
Mathew Robinson <chasinglogic@gmail.com>
2020-02-09 10:00:45 -0500

    Add get_suffix to node class

--- bs/node.py
+++ bs/node.py
@@ -1,7 +1,15 @@
+from bs.util import get_suffix
+
+
 class Node:
-    def __init__(self, name, children=None):
+    """Node is a target or source file in the graph."""
+
+    def __init__(self, filename, children=None):
         if children is None:
             children = []
 
-        self.name = name
+        self.filename = filename
         self.children = children
+
+    def get_suffix(self):
+        return get_suffix(self.filename)
